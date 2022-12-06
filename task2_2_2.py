d284a54114d7f813f3cecaeaf2def4f00c206489
Pavel Vakhrin <ratchet23122003@gmail.com>
2022-12-06 18:50:08 +0500

    2-2-3 сделано

--- task2_2_2.py
+++ task2_2_2.py
@@ -3,11 +3,7 @@ from task2_1_3 import get_statistics
 
 request = input()
 
-<<<<<<< HEAD
 #ветка main
-=======
-#изменение в develop
->>>>>>> develop
 
 if request == 'Вакансии':
     get_vacancies_table()
